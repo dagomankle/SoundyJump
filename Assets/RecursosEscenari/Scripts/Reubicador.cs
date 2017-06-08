bcec5fd9ce7d3138791618babd0a8c0de34cebc2
Dago <dagomankle@hotmail.com>
2017-06-08 14:34:34 -0500

    Scena de fondo

---
+++ Assets/RecursosEscenari/Scripts/Reubicador.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reubicador : MonoBehaviour {
+
+    public float ixPos= 0.0f;
+    public float fxPos = 0.0f;
+
+    void SetTransformX()
+    {
+        gameObject.transform.position = new Vector3(ixPos, transform.position.y, transform.position.z);
+        print(transform.position.y);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (transform.position.x <= fxPos)
+        {
+            SetTransformX();
+        }
+    }
+}
