7cef80bc30249f55b9e1ac2141310a31c307dd1c
RonnyCG <ronny1612695@gmail.com>
2017-06-08 16:35:28 -0500

    Se agrego los obstaculos y el algoritmo de generacion de los mismos.

---
+++ Assets/Obstaculos/Scripts/generarObstaculo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generarObstaculo : MonoBehaviour {
+
+    public GameObject[] obs;
+    public string partitura;
+    public float tiempoMin = 1f;
+    public float tiempoMax = 1f;
+    
+    // Use this for initialization
+    void Start()
+    {
+       // int size = partitura.Split(',').Length;
+        generar();
+        
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+       // Invoke("generar", Random.Range(tiempoMin, tiempoMax));
+    }
+
+    void generar()
+    {
+
+        Instantiate(obs[Random.Range(0, obs.Length)], transform.position, Quaternion.identity);
+        Invoke("generar", Random.Range(tiempoMin, tiempoMax));
+    }
+}
---
+++ Assets/Obstaculos/Scripts/movimientoObstaculo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoObstaculo : MonoBehaviour {
+
+    // Use this for initialization
+   
+    public float velocidad = 0.5f;  
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        transform.position = new Vector3(transform.position.x - velocidad,  transform.position.y, transform.position.z   );
+	}
+}
